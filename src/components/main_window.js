6aa4bc23f6f63b3b0cff7b03358865fcf56f76c4
BugQualia <jjpark@postech.ac.kr>
2022-11-20 19:56:36 +0900

    1 file modified

--- src/components/main_window.js
+++ src/components/main_window.js
@@ -1,29 +1,35 @@
 import React from 'react';
 
 const Workbench = (props) => {
-  const [writemode, setWritemode] = React.useState(false);
-  const regInput = React.useRef();
+  const highlightMe = React.useRef();
+  const highlightColor = React.useRef();
 
   const Highlight = () => {
-    const string = regInput.current.value;
+    const string = highlightMe.current.value;
+    const color = highlightColor.current.value;
+    if (string === '') {
+      return;
+    }
+
     const simpletext = new RegExp('(' + string + ')', 'gi');
 
     var textHigh = document.getElementById('text');
     textHigh.innerHTML = textHigh.innerHTML.replace(
       simpletext,
-      '<span style="color: red">$1</span>'
+      '<span style="background: ' + color + '">$1</span>'
     );
   };
 
   return (
     <div>
-      <input ref={regInput}></input>
+      <input ref={highlightMe} placeholder="text"></input>
+      <input ref={highlightColor} placeholder="color"></input>
       <button onClick={Highlight}>aaaa!</button>
       <div
         id="text"
-        contenteditable="true"
+        contentEditable="true"
         aria-disabled="false"
-        spellcheck="true"
+        spellCheck="true"
       >
         <p>aaaaaaaa</p>
         <p>asd asd replace</p>
